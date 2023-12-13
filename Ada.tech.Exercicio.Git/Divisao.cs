cf1fb0ec02c675e580af152405e5157dc52f6e30
EA-Igor <igordrillpsn@gmail.com>
2023-12-13 16:04:30 -0300

    Foi adicionado a operação de divisão e subtração

---
+++ Ada.tech.Exercicio.Git/Divisao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ada.tech.Exercicio.Git
+{
+    internal class Divisao
+    {
+        public decimal efetuarDivisao(Decimal x, Decimal y)
+        {           
+            if (y==0)
+            {
+                throw new DivideByZeroException("Divisão por zero não é permitida.");
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+    }
+}
