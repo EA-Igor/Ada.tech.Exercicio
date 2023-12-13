b9a4059c88a966e69cb244e4e301871ccd9e6d2c
Miguel Pereira de Sousa <miguel.pereira.12@live.com>
2023-12-13 16:01:10 -0300

    Adicionado a classe subtração

---
+++ Ada.tech.Exercicio.Git/subtracao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ada.tech.Exercicio.Git
+{
+    internal class Subtracao
+    {
+        public decimal EfetuarSubtracao(decimal x, decimal y)
+        {
+            return  x - y;
+        }
+    }
+}
