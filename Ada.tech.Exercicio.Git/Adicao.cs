64756be46994344c466e975659d3b6f5902ef4df
AmandaaBastos <amandabastoscorreia@gmail.com>
2023-12-13 16:21:23 -0300

    Adc adição

---
+++ Ada.tech.Exercicio.Git/Adicao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ada.tech.Exercicio.Git
+{
+    internal class Adicao
+    {
+        public decimal efetutuarAdicao(Decimal x, Decimal y)
+        {
+            return x + y;
+        }
+
+
+    }
+}
