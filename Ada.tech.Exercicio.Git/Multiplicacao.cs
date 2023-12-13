11253326db91a51293738e23d5c57a08a6d413d3
cristopherkovalski <cristopherkovalski@gmail.com>
2023-12-13 16:18:14 -0300

    refact

--- Ada.tech.Exercicio.Git/Multiplicacao.cs
+++ Ada.tech.Exercicio.Git/Multiplicacao.cs
@@ -8,5 +8,10 @@ namespace Ada.tech.Exercicio.Git
 {
     internal class Multiplicacao
     {
+        public decimal Multiplica(decimal num, decimal num2)
+        {
+            decimal resultado = num * num2;
+            return resultado;
+        }
     }
 }
