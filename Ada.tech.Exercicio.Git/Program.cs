86e952498498b5ca23e3ee48302d71d1848fd0dd
EA-Igor <igordrillpsn@gmail.com>
2023-12-13 15:52:18 -0300

    Foi adicionado a mensagem inicial da calculadora.

--- Ada.tech.Exercicio.Git/Program.cs
+++ Ada.tech.Exercicio.Git/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+decimal x, y;
+
+Console.WriteLine("Bem-vindo a calculadora! ");
+Console.WriteLine("Insira os números para realizar as operações: ");
+decimal.TryParse(Console.ReadLine(), out x);
+decimal.TryParse(Console.ReadLine(), out y);
